ce248d484c427912d6ea412928e836838613ff2b
Sami Al-Dury <git@aldury.com>
2023-08-03 21:11:23 +0200

    feat: semantic release

---
+++ release.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [
+                    { type: 'refactor', release: 'patch' },
+                    { scope: 'chore', release: 'patch' },
+                ],
+            },
+        ],
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'CHANGELOG.md'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]',
+            },
+        ],
+        '@semantic-release/github',
+    ],
+}
