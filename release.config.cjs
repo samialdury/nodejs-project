148eb6c70b62e8fc1d2333e4fd436cc05cf3761f
Sami Al-Dury <git@aldury.com>
2023-08-03 21:23:41 +0200

    feat: semantic release npm plugin

--- release.config.cjs
+++ release.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
         ],
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
+        '@semantic-release/npm',
         [
             '@semantic-release/git',
             {
