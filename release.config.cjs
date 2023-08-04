0dd1798b3b06f02c7f359f71858bcae1b75a01ea
Sami Al-Dury <git@aldury.com>
2023-08-04 12:06:36 +0200

    feat: update deps

--- release.config.cjs
+++ release.config.cjs
@@ -1,26 +1,3 @@
-module.exports = {
-    branches: ['main'],
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'angular',
-                releaseRules: [
-                    { type: 'refactor', release: 'patch' },
-                    { scope: 'chore', release: 'patch' },
-                ],
-            },
-        ],
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/npm',
-        [
-            '@semantic-release/git',
-            {
-                assets: ['package.json', 'CHANGELOG.md'],
-                message: 'chore(release): ${nextRelease.version} [skip ci]',
-            },
-        ],
-        '@semantic-release/github',
-    ],
-}
+const { semanticRelease } = require('@samialdury/config')
+
+module.exports = semanticRelease.config()
