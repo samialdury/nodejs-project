d333b0b691ffce4e11c657e6affad1b28eee01fe
Sami Al-Dury <git@aldury.com>
2023-08-05 15:37:08 +0200

    fix(ci): do not skip-ci on new version commit

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,5 @@
 const { semanticRelease } = require('@samialdury/config')
 
-module.exports = semanticRelease.config()
+module.exports = semanticRelease.config({
+    message: 'chore(release): v${nextRelease.version}',
+})
