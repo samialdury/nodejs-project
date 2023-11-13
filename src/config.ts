a147f38a4985bfac06887ef608a06e12d932afe6
Sami Al-Dury <git@aldury.com>
2023-11-13 19:57:33 +0100

    refactor: default config values

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,39 @@
 import { type EnveySchema, type InferEnveyConfig, createConfig } from 'envey'
 import { z } from 'zod'
+import { BaseError } from './base-error.js'
 
 const schema = {
     commitSha: {
         env: 'COMMIT_SHA',
-        format: z.string(),
+        format: z.string().default('unknown'),
     },
     logLevel: {
         env: 'LOG_LEVEL',
-        format: z.enum([
-            'fatal',
-            'error',
-            'warn',
-            'info',
-            'debug',
-            'trace',
-            'silent',
-        ]),
+        format: z
+            .enum([
+                'fatal',
+                'error',
+                'warn',
+                'info',
+                'debug',
+                'trace',
+                'silent',
+            ])
+            .default('info'),
     },
     projectName: {
         env: 'PROJECT_NAME',
-        format: z.string(),
+        format: z.string().default('nodejs-project'),
     },
 } satisfies EnveySchema
 
 export type Config = InferEnveyConfig<typeof schema>
+
+class ConfigError extends BaseError {
+    constructor(message: string) {
+        super(message, false)
+    }
+}
 
 export function initConfig(): Config {
     const result = createConfig(z, schema, { validate: true })
@@ -32,7 +41,7 @@ export function initConfig(): Config {
     if (!result.success) {
         // eslint-disable-next-line no-console
         console.error(result.error.issues)
-        throw new Error('Invalid config')
+        throw new ConfigError('Invalid configuration')
     }
 
     return result.config
